6f36dd60a12b42b703caf43927c854131779ba19
Megan McCoy <mnmccoy95@gmail.com>
2020-10-02 12:37:08 -0500

    first commit

---
+++ src/components/ApplicationViews.js
@@ -0,0 +1,38 @@
+import React from "react"
+import { Route } from "react-router-dom"
+import { Home } from "./Home"
+import { AnimalCard } from "./animal/AnimalCard"
+import { LocationCard } from "./location/LocationCard"
+import { EmployeeCard } from "./employee/EmployeeCard"
+import { CustomerCard } from "./customer/CustomerCard"
+
+export const ApplicationViews = (props) => {
+    return (
+        <>
+            {/* Render the location list when http://localhost:3000/ */}
+            <Route exact path="/">
+                <Home />
+            </Route>
+
+            {/* Render the animal list when http://localhost:3000/animals */}
+            <Route path="/animals">
+                <AnimalCard />
+            </Route>
+
+            {/* Render the animal list when http://localhost:3000/locations */}
+            <Route path="/locations">
+                <LocationCard />
+            </Route>
+
+            {/* Render the animal list when http://localhost:3000/employees */}
+            <Route path="/employees">
+                <EmployeeCard />
+            </Route>
+
+            {/* Render the animal list when http://localhost:3000/customers */}
+            <Route path="/customers">
+                <CustomerCard />
+            </Route>
+        </>
+    )
+}
